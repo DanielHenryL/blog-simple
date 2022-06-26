d30d7855c4582c584c17b430abd5972e0fd6cba3
HenryDani <lagunasdaniel001@gmail.com>
2022-06-25 21:39:10 -0500

    Vistas del sitio CRUD

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,20 +1,53 @@
 from django.shortcuts import render
-from django.views.generic import ListView, View
-from .models import Post
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView, View
+from .models import Post , User, Comment, Like, PostView
 # Create your views here.
 
-
-class listView(View):
+class HomeView(View):
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all()
-        context ={
-            'posts':posts,
-        }
-        return render(request, 'post_list.html',context)
+        context = {
 
-        
-# class listview(ListView):
-#     model = Post
-#     context_object_name = 'posts'
-#     template_name = 'post_list.html'
+        }
+        return render(request, 'base.html', context)
+
+class PostListView(ListView):
+    model = Post
+    tamplate_name = 'posts/post_list.html'
+    context_object_name = 'posts'
+    
+
+class PostDetailView(DetailView):
+    model = Post
+    tamplate_name = 'posts/post_list.html'
+    context_object_name = 'post'
+    
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'posts/post_create.html'
+    success_url = reverse_lazy('posts:post_list')
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug'
+    )
+    
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'posts/post_form.html'
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug'
+    )
+    context_object_name = 'post'
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('posts:post_list')
+    
     
