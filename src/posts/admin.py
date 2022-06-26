daa629771fadcef3149d12c3104e1b1786896e60
HenryDani <lagunasdaniel001@gmail.com>
2022-06-25 19:55:56 -0500

    Personalizando nuestro usuario con django-allauth

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, PostView, Like, Comment
+from .models import Post, PostView, Like, Comment, User
 admin.site.register(Post)
 admin.site.register(PostView)
 admin.site.register(Like)
 admin.site.register(Comment)
+admin.site.register(User)
 # Register your models here.
