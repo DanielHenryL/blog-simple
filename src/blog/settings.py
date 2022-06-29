67f995298c3243e575e1c065a21314c29f73d638
HenryDani <lagunasdaniel001@gmail.com>
2022-06-28 19:03:49 -0500

    hola

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
